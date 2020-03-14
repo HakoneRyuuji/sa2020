d7b338ef803b347045415968a51532f186bc48db
nguyenducquyen <hakonaryuuji@gmail.com>
2020-03-14 09:10:07 +0700

    Add the Labwork 05: 06.practical.work.shell...

---
+++ 05.Labwork/06.practical.work.shell.history.c
@@ -0,0 +1,113 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include<unistd.h>
+
+#include<string.h>
+
+#include<stdbool.h>
+
+#include<sys/wait.h>
+
+void handler(int signal_num) {
+  printf("Signal %d => ", signal_num);
+  switch (signal_num) {
+  case SIGTSTP:
+    printf("pause\n");
+    break;
+  case SIGINT:
+  case SIGTERM:
+    printf("Terminated\n");
+    exit(0);
+    break;
+  }
+}
+
+int main(int argc, char const *argv[]) {
+
+    FILE *f = fopen("command.log", "w");
+	char input[100];
+	char *args[10];
+    pid_t pid;
+    char cmd[99];
+  // ctrl z
+    signal(SIGTSTP, handler);
+  // ctrl c or killed
+    signal(SIGINT, handler);
+    signal(SIGTERM, handler);
+
+	while (1) {
+		// ask for command
+		printf("Enter your command:");
+		fgets(input, sizeof(input), stdin);
+
+        fprintf(f, "Input command: %s", input); // input to command.log file
+
+		// initialization of args everytime
+		memset(args, 0, sizeof(args));
+		
+		// transform the input string
+		// to array of args
+		// so that execvp can use
+		int argc = 0;
+		int len = strlen(input);
+		char *prevArg = input;
+		for (int i = 0; i < len; i++) {
+			if (input[i] == ' ') {
+				args[argc++] = prevArg;
+				prevArg = &input[i+1];
+				input[i] = '\0';
+			}
+			if (input[i] == '\n') {
+				input[i] = '\0';
+			}
+		}
+		args[argc++] = prevArg;
+
+		// dump the info for debugging purpose
+		printf("Input : %s\n", input);
+		if (strcmp(input, "quit") == 0) {
+			break;
+		}
+
+//when quit, display the history
+        if (strcmp(input, "quit") == 0) {
+			//// start pw 5
+			fclose(f);
+			FILE *f = fopen("command.log", "r");
+			char line[100];			
+			printf("Command history is:\n");
+			while (1) {
+				memset(line, 0, sizeof(line));
+				fgets(line, sizeof(line), f);
+				if (strlen(line) == 0) {
+					break;
+				}
+				printf(" + %s", line);
+			}
+			fclose(f);
+			//// end pw 5
+			break;
+		}
+
+		printf("- argc : %d\n", argc);
+		printf("- args : \n");
+		for (int i = 0; i <= argc; i++) {
+			printf("  + args[%d]=%s\n", i, args[i]);
+		}
+
+		// fork() + exec() combo
+		int pid = fork();
+		if (pid == 0) {
+			execvp(args[0], args);
+		}
+		else {
+			waitpid(pid, NULL, 0);
+		}
+	}
+	printf("Terminating.\n");
+    
+	return 0;
+}
+
